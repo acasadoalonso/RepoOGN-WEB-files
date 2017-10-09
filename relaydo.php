e8524ef64e41544e0d0fd0129d0efb13ddc27398
Angel Casado <acasadoalonso@gmail.com>
2017-10-09 19:22:06 +0200

    handling todays date

--- relaydo.php
+++ relaydo.php
@@ -6,11 +6,23 @@ $Filename='DATA'.$yy.$mm.$dd.'.log';
 $cwd =getcwd();
 $rc=0;
 echo 'Filename='.$Filename.'<br>';
+$DD=date('d');
+$MM=date('m');
+$YY=date('y');
 // Change directory
-chdir("/nfs/OGN/DIRdata/data/");
-
+if ($dd == $DD and $mm == $MM and $YY == $yy)
+	{
+	chdir("/nfs/OGN/DIRdata/");
+	$pgm ="../src/analysisrelay.py";
+	}
+else
+	{
+	chdir("/nfs/OGN/DIRdata/data/");
+	$pgm ="../../src/analysisrelay.py";
+	}
+//echo getcwd();
 ob_start();
-passthru('/usr/bin/python2.7 ../../src/analysisrelay.py -sa YES -n '.$Filename.' ', $rc);
+passthru('/usr/bin/python2.7 '.$pgm.' -sa YES -n '.$Filename.' ', $rc);
 $output = ob_get_clean(); 
 echo nl2br($output);
 ?>
