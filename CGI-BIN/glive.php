b5974434453c78da1ad3d2d32c3d3d8c0dab8932
Angel Casado <acasadoalonso@gmail.com>
2022-08-31 18:23:19 +0200

    fix set SQL commands to adapt it to the new version

--- CGI-BIN/glive.php
+++ CGI-BIN/glive.php
@@ -2,26 +2,26 @@
 include 'config.php';
 include 'config.php';
 $regis = $_POST['regis'];
-if ($secretKey != '') {
-	if(isset($_POST['g-recaptcha-response'])){
-          $captcha=$_POST['g-recaptcha-response'];
-        }
-	if(!$captcha){
-          echo '<h2>Please check the the captcha form.</h2>';
-          exit;
-        }
-
-	$ip = $_SERVER['REMOTE_ADDR'];
-        // post request to server
-	$url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
-	$response = file_get_contents($url);
-	$responseKeys = json_decode($response,true);
-        // should return JSON with success as true
-	if(!$responseKeys["success"]) {
-                echo '<h2>You are spammer ! Get the @$%K out</h2>';
-                die;
-       }
-}
+//if ($secretKey != '') {
+	//if(isset($_POST['g-recaptcha-response'])){
+          //$captcha=$_POST['g-recaptcha-response'];
+        //}
+	//if(!$captcha){
+          //echo '<h2>Please check the the captcha form.</h2>';
+          //exit;
+        //}
+//
+	//$ip = $_SERVER['REMOTE_ADDR'];
+        //// post request to server
+	//$url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
+	//$response = file_get_contents($url);
+	//$responseKeys = json_decode($response,true);
+        //// should return JSON with success as true
+	////if(!$responseKeys["success"]) {
+         ////       echo '<h2>You are spammer ! Get the @$%K out</h2>';
+          ////      die;
+       ////}
+//}
 $cwd =getcwd();
 $rc=0;
 #echo 'regis='.$regis.'<br>';
