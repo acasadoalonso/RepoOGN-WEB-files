29adbadb03d505e19a25192fc44c50f3db2cfc74
acasadoalonso <acasado@acm.org>
2024-07-29 10:04:30 +0200

    delete the captcha

--- CGI-BIN/gbydate.php
+++ CGI-BIN/gbydate.php
@@ -4,29 +4,11 @@ $dd = $_POST['dd'];
 $mm = $_POST['mm'];
 $yy = $_POST['yy'];
 $hostname = gethostname(); 
-if ($secretKey != '' and $hostname != 'CasadoUbuntu') {
 
-	if(isset($_POST['g-recaptcha-response'])){
-          $captcha=$_POST['g-recaptcha-response'];
-        }
-	if(!$captcha){
-          echo '<h2>Please check the the captcha form.</h2>';
-          exit;
-        }
-	$ip = $_SERVER['REMOTE_ADDR'];
-        // post request to server
-	$url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
-	$response = file_get_contents($url);
-	$responseKeys = json_decode($response,true);
-        // should return JSON with success as true
-	if(!$responseKeys["success"]) {
-                echo '<h2>You are spammer ! Get the @$%K out</h2>';
-                die;
-       }
-}
+$ip = $_SERVER['REMOTE_ADDR'];
 $cwd =getcwd();
 $rc=0;
-echo 'Date='.$dd.$mm.$yy.'<br>';
+echo 'Date='.$dd.$mm.$yy.' '.$ip.'<br>';
 //echo getcwd();
 ob_start();
 
