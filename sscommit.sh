9ff172acf3b363f9bd0693c8b51b8fdc8e3246bb
acasadoalonso <acasado@acm.org>
2025-08-08 10:27:51 +0200

    .

--- sscommit.sh
+++ sscommit.sh
@@ -1,7 +1,7 @@
-rm *funcs.py  CGI-BIN/*funcs.py
+rm *funcs.py  CGI-BIN/*funcs.py CGI-BIN/ksta.py
 cp /nfs/OGN/src/funcs/parserfuncs.py CGI-BIN
-cp /nfs/OGN/src/funcs/ksta.py        CGI-BIN
 cp /nfs/OGN/src/funcs/dtfuncs.py     CGI-BIN
+cp /nfs/OGN/src/funcs/ksta.py        CGI-BIN
 git add .
 git commit
 git push origin master
