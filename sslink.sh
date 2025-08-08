832c2388193befa2426a17738d9be391558f45b0
acasadoalonso <acasado@acm.org>
2025-08-08 09:56:51 +0200

    sync

--- sslink.sh
+++ sslink.sh
@@ -1,4 +1,5 @@
 rm CGI-BIN/*funcs.py 
 ln -s /nfs/OGN/src/funcs/parserfuncs.py CGI-BIN
 ln -s /nfs/OGN/src/funcs/dtfuncs.py     CGI-BIN
+ln -s /nfs/OGN/src/funcs/ksta.py        CGI-BIN
 ls -la CGI-BIN/*funcs.py
